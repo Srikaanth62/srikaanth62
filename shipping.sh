d4ccbe7364e1fba57a27cdc46edc2337fa31a1f7
srikaanth <srikaanth62@gmail.com>
2023-04-22 10:52:04 +0530

    cart common

--- shipping.sh
+++ shipping.sh
@@ -2,31 +2,31 @@ path=$(realpath "$0")
 script_path=$(dirname "$path")
 source ${script_path}/common.sh
 mysql_root_pwd=$1
-echo -e "\e[31m>>>>>>>>>>> Install java packaging software<<<<<<<<<\e[0m"
+print_head Install java packaging softwareprint_head
 yum install maven -y
-echo -e "\e[31m>>>>>>>>>> create application user<<<<<<<<<\e[0m"
+print_head create application user<<<<<<<<<\e[0m"
 useradd ${add_user}
-echo -e "\e[31m>>>>>>>>>> craete app directory<<<<<<<<<\e[0m"
+print_head craete app directoryprint_head
 rm -rf /app
 mkdir /app
-echo -e "\e[31m>>>>>>>>>> Download application code <<<<<<<<<\e[0m"
+print_head Download application code <<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
-echo -e "\e[31m>>>>>>>>>> unzip code file<<<<<<<<<\e[0m"
+print_head unzip code fileprint_head
 unzip /tmp/shipping.zip
-echo -e "\e[31m>>>>>>>>>> download dependencies and build the applications<<<<<<<<<\e[0m"
+print_head download dependencies and build the applications<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[31m>>>>>>>>>> copy shipping service <<<<<<<<<\e[0m"
+print_head copy shipping service print_head
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[31m>>>>>>>>>> Load service <<<<<<<<<\e[0m"
+print_head Load service <<<<<<<<<\e[0m"
 systemctl daemon-reload
-echo -e "\e[31m>>>>>>>>>> Enable and start the service <<<<<<<<<\e[0m"
+print_head Enable and start the service print_head
 systemctl enable shipping
 systemctl start shipping
-echo -e "\e[31m>>>>>>>>>> Install sql client <<<<<<<<<\e[0m"
+print_head Install sql client <<<<<<<<<\e[0m"
 yum install mysql -y
-echo -e "\e[31m>>>>>>>>>> Load the sql schema <<<<<<<<<\e[0m"
+print_head Load the sql schema print_head
 mysql -h mysql-dev.srikaanth62.online -uroot -p${mysql_root_pwd} < /app/schema/shipping.sql
-echo -e "\e[31m>>>>>>>>>> Restart the service <<<<<<<<<\e[0m"
+print_head Restart the service <<<<<<<<<\e[0m"
 systemctl restart shipping
